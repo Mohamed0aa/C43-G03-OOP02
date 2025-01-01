7b713bb61655718531cea4c28b9ac21012288e59
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-01-01 19:13:09 +0200

    v1

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region vedio 1
+            point p1;//object  of type point
+            //clr will allocate 8 un intailized byts in stack
+
+            p1 = new point();//paramter  less constructor =>[defult constructor]
+
+            p1 = new point(1,3); //new is initailize constructor
+
+            p1.x = 1;
+            p1.y = 2;
+            Console.WriteLine(p1.x);
+            Console.WriteLine(p1.y);
+            Console.WriteLine(p1.ToString());//=>name space +datatype
+            #endregion
         }
     }
 }
