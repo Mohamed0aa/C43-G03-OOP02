ef28653a4cded51181ceaa3195168bf80d7f18bf
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-01-02 18:26:00 +0200

    v8

---
+++ Demo/child.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class child :parent
+    {
+        public int z {  get; set; }
+        public child(int x,int y, int z) :base(x,y)
+        {
+            this.z = z;
+        }
+
+        public override string ToString()
+        {
+             return $"x= {X} y={Y} z={z}";
+        }
+
+        public new int product()
+        {
+            return X * Y*z;
+        }
+    }
+}
