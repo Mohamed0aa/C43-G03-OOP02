44a4fcde46dc0985775ce527678c9068fbe37469
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-01-03 01:02:17 +0200

    q5

---
+++ assignment/Date.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace assignment
+{
+    internal class Date
+    {
+        public int year { get; set; }
+        public int month { get; set; }
+        public int day { get; set; }
+
+        public Date(int year, int month, int day)
+        {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+        }
+        public override string ToString()
+        {
+            return $"day = {day}\n month = {month}\n yeat ={year}";
+        }
+    }
+}
