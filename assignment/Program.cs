e5d2d35e3fd3960b9acdc65ef748da1855e1b4f4
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-01-02 23:02:13 +0200

    q1

---
+++ assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region q1 : Define a struct "Person" with properties "Name" and "Age". Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
+            //Person[] persons = new Person[3];
+            //persons[0]=new Person();
+            //persons[0].name = "mohamed";
+            //persons[0].age = 21;
+            //persons[1]=new Person();
+            //persons[1].name = "ahmed";
+            //persons[1].age = 19;
+            //persons[2] = new Person();
+            //persons[2].name = "mahmoud";
+            //persons[2].age = 18;
+            //for (int i = 0; i < persons.Length; i++)
+            //{
+            //    Console.WriteLine($"{i+1}:");
+            //    Console.WriteLine($"{persons[i]}");
+            //}
+            #endregion
+        }
+    }
+}
